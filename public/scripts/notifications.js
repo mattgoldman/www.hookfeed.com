a1e7224e5ce0dbc510ce50d2a9e8d76dfe5534d5
Matt Goldman <matt@smallhq.com>
2013-04-28 18:03:03 -0700

    everything wrapped except notification editing

--- public/scripts/notifications.js
+++ public/scripts/notifications.js
@@ -1,8 +1,93 @@
-$(document).ready(function(){
-	$notifications_container = $('#notifications');
-	$queued_notifications = $('.queued_notification');
+notificationLinkClicked = function(event){
+	event.stopPropagation();
 
-	setInterval(function(){
+	alert('We get it, you think this is awesome. But it\'s just a teaser page. If you want the real deal, reserve your spot below.');
+
+	return false;
+};
+
+$(document).ready(function(){
+	var $notifications_container = $('#notifications');
+	var $queued_startup_notifications = $('.queued_notification.startups');
+	var $queued_developer_notifications = $('.queued_notification.developers');
+	var $queued_marketer_notifications = $('.queued_notification.marketers');
+	var $queued_freelancer_notifications = $('.queued_notification.freelancers');
+	var $queued_notifications = $queued_startup_notifications;
+	var currentUseCase = 'startups';
+
+	// Notifications Navigation
+	$navLinks = $('a', '#navigation nav');
+	$navLinks.on('click', function(){
+		event.stopPropagation();
+
+		oldUseCase = currentUseCase;
+		currentUseCase = $(this).text().toLowerCase();
+
+		if(oldUseCase != currentUseCase){
+			// Select new button
+			$navLinks.removeClass('selected');
+			$(this).addClass('selected');
+
+
+			$notifications = $('.notification', $notifications_container);
+			numNotifications = $notifications.length;
+
+			// Slide remaining notifications
+			$notifications.animate({
+				top: '+=400px',
+				opacity: 0
+			},
+			{
+				duration: 500,
+				easing: 'easeOutExpo',
+				complete: function(){
+	 				// Only run this callback once at the end
+	 				if( --numNotifications > 0 ) return;
+
+					// Add new notification to the top
+					addNotification();
+
+					//CLEAR TIMEOUT? TODO
+				}
+			});
+		}
+
+		return false;
+	});
+
+	// Bind clicks on first 3 notifications
+	$('.notification').on('click', 'a', notificationLinkClicked);
+
+	nextNotification = function(){
+		if(currentUseCase == 'startups'){
+			$queued_notifications = $queued_startup_notifications;
+		}else if(currentUseCase == 'developers'){
+			$queued_notifications = $queued_developer_notifications;
+		}else if(currentUseCase == 'marketers'){
+			$queued_notifications = $queued_marketer_notifications;
+		}else if(currentUseCase == 'freelancers'){
+			$queued_notifications = $queued_freelancer_notifications;
+		}
+		html = "<div class=\"notification\" style=\"opacity: 0\">";
+		html += $($queued_notifications[Math.floor(Math.random()*$queued_notifications.length)]).first().html();
+		html += "</div>";
+
+		return html;
+	}
+
+	addNotification = function(){
+		$notifications_container.prepend(nextNotification());
+
+		$('.notification', $notifications_container).first().animate({
+			opacity: 1
+		},
+		{
+			duration: 500,
+			easing: 'easeOutCirc'
+		}).on('click', 'a', notificationLinkClicked);
+	}
+
+	cycleNotifications = function(){
 		$notifications = $('.notification', $notifications_container);
 		numNotifications = $notifications.length;
 
@@ -26,22 +111,19 @@ $(document).ready(function(){
 	 			// Only run this callback once at the end
 	 			if( --numNotifications > 0 ) return;
 
-		 		// Remove last notification
-		 		$notifications.last().remove();
+		 		// Remove last notification and unbind click event
+		 		$notifications.last().off('click', 'a', notificationLinkClicked).remove();
 
 				// Add new notification to the top
-				html = "<div class=\"notification\" style=\"opacity: 0\">";
-				html += $($queued_notifications[Math.floor(Math.random()*$queued_notifications.length)]).first().html();
-				html += "</div>";
-				$notifications_container.prepend(html);
-				$('.notification', $notifications_container).first().animate({
-					opacity: 1
-				},
-				{
-					duration: 500,
-					easing: 'easeOutCirc'
-				});
+				addNotification();
 		 	}
 		});
-	}, 3750);
+
+		millisecondsOptions = [3400, 3000, 2500, 3200, 2800, 2600, 3500];
+		randomMilliseconds = millisecondsOptions[Math.floor(Math.random() * millisecondsOptions.length)];
+
+		setTimeout(cycleNotifications, randomMilliseconds);
+	}
+
+	setTimeout(cycleNotifications, 2500);
 });
